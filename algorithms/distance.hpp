fc3de3d3f5b7fa5b9b1c7e32d6a1e551679a889f
vahanafk <vahan.ghazaryan.00@mail.ru>
2024-07-01 13:27:33 +0400

    added distance fucntion for iterators

---
+++ algorithms/distance.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace ft
+{
+    template< typename InputIt >
+    typename InputIt::difference_type \
+    distance(InputIt first, InputIt last)
+    {
+        typename InputIt::size_type count = 0;
+        while (first != last) {
+            first++;
+            count++;
+        }
+        return (count);
+    };
+}
